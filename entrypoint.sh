aa3f06991bd1f94f39b3fe3729241d68818b0954
privdev85 <54279019+privdev85@users.noreply.github.com>
2020-03-20 11:07:34 +0100

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,8 @@ echo "Starting with Group ID: $GROUP_ID"
 groupadd -g ${GROUP_ID} datascience
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -l -u $USER_ID -g datascience -o -c "" -m developer
-export HOME=/home/developer
-source $HOME/.aws/mfa_session
 
-# virtual environment
-export VIRTUAL_ENV=$HOME/venv
-export PATH=$VIRTUAL_ENV/bin:$PATH
+
 
 # execute with gosu to "get rid" of root
 gosu developer mkdir -p $VIRTUAL_ENV
@@ -37,11 +33,4 @@ gosu developer pip install numpy \
                            XlsxWriter \
                            seaborn \
                            s3fs
-
-if test -f "/home/developer/python_requirements.txt"; then
-    echo "Found requirements file"
-    gosu developer pip install -r python_requirements.txt
-else
-    echo "NO requirements file found"
-fi
 
